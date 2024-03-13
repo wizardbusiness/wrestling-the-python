1782c1617928974b19259db4f809823df757215d
Gabriel <gkime91@gmail.com>
2024-03-13 10:37:43 -0700

    python mergesort

--- mergesort.py
+++ mergesort.py
@@ -12,14 +12,13 @@ def mergesort(array):
 
 def merge(l, r):
   sorted = []
-  while len(l) and len(r):
+  while(len(l) and len(r)):
     if l[0] < r[0]:
-      sorted.append((l[0]))
-      del l[0]
-    else: 
-      sorted.append(r[0])
-      del r[0]
-
+      sorted.append(l.pop(0))
+    else:
+      sorted.append(r.pop(0))
+  print(sorted)
   return [*sorted, *l, *r]
+  
 
 print(mergesort(unsorted))
